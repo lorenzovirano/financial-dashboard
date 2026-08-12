89a47db8150a380d7cf5048a3fb5d05f81f5d098
Lorenzo Virano <lorenzovirano1702@gmail.com>
2026-08-12 17:40:25 +0200

    refactor: decouple frontend to isolate Fastify backend API

--- backend/routes/banks.routes.js
+++
@@ -1,9 +0,0 @@
-const banksController = require('../controllers/banks.controller');
-
-const express = require("express");
-const router = express.Router();
-
-router.post("/import", banksController.import);
-router.get("/get-bank", banksController.getBank);
-
-module.exports = router;
